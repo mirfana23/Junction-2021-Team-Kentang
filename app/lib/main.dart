f3602a4a02a809364421b51dbd5c62c97abc2905
jowillianto <jowillianto@kaist.ac.kr>
2021-11-20 15:23:39 +0900

    Add : base for flutter app

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,24 +1,15 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
+import './pages/home.dart';
+import './pages/stats.dart';
+import './pages/forecast.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      home  : Home(),
     );
   }
 }
--- app/lib/pages/stats.dart
+++ app/lib/pages/stats.dart
@@ -1,24 +1,12 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
+class Stats extends StatelessWidget{
+  final String title = 'Stats';
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar  : AppBar(title : Text(this.title)),
+      body    : Center(child : Text('Hello' + this.title)),
     );
   }
 }
