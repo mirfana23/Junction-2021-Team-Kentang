c450e7d099db9f45d88e2fa4968cdd950cad4fdb
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2021-11-20 17:49:50 +0900

    forecast

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,19 +11,16 @@ import './core/topbar.dart';
 
 void main() => runApp(App());
 
-class App extends StatelessWidget{
-  final String appTitle   = "Kentang App";
-  final routing       = {
-    '/'         : (context) => const Home(),
-    '/stats'    : (context) => Stats(),
-    '/forecast' : (context) => Forecast()
+class App extends StatelessWidget {
+  final String appTitle = "Kentang App";
+  final routing = {
+    '/': (context) => const Forecast(),
+    '/stats': (context) => Stats(),
+    '/forecast': (context) => Forecast()
   };
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title         : this.appTitle, 
-      initialRoute  : '/',
-      routes        : this.routing
-    );
+        title: this.appTitle, initialRoute: '/', routes: this.routing);
   }
 }
--- app/lib/pages/forecast.dart
+++ app/lib/pages/forecast.dart
@@ -1,12 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class Forecast extends StatelessWidget{
+class Forecast extends StatelessWidget {
+  const Forecast({Key? key}) : super(key: key);
   final String title = 'Forecast';
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar  : AppBar(title : Text(this.title)),
-      body    : Center(child : Text('Hello' + this.title)),
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      home: MainScreen(),
     );
+  }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(
+          middle: Text('Sample Code'),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(top: 128),
+              padding: EdgeInsets.only(bottom: 8),
+              child: Image.asset(
+                'images/save_money.png',
+                width: 200,
+                height: 100,
+                fit: BoxFit.contain,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 128),
+              padding: EdgeInsets.only(bottom: 8),
+              child: Text(
+                'Save your money',
+              ),
+            )
+          ],
+        ));
   }
 }
