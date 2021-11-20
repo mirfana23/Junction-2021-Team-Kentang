b8a87acacb75511f496c7cc8e7269c8e4ccefd03
jowillianto <jowillianto@kaist.ac.kr>
2021-11-20 15:34:12 +0900

    Routing

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -6,10 +6,18 @@ import './pages/forecast.dart';
 void main() => runApp(App());
 
 class App extends StatelessWidget{
+  final String appTitle   = "Kentang App";
+  final routing       = {
+    '/'         : (context) => const Home(),
+    '/stats'    : (context) => Stats(),
+    '/forecast' : (context) => Forecast()
+  };
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      home  : Home(),
+      title         : this.appTitle, 
+      initialRoute  : '/',
+      routes        : this.routing
     );
   }
 }
--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget{
+  const Home({Key? key}) : super(key: key);
   final String title = 'Home';
   @override
   Widget build(BuildContext context){
