e54523a7d77d131a08e74c64e533b4a2aeb095df
reinattwijaya <71814354+reinattwijaya@users.noreply.github.com>
2021-11-21 14:33:06 +0900

    delete

---
+++ delete/lib/widgets/discover_long_card.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:meditation/icons.dart';
+import 'package:meditation/widgets/svg_asset.dart';
+
+class DiscoverLongCard extends StatelessWidget {
+  final String? title;
+  final String? subtitle;
+  final Color? gradientStartColor;
+  final Color? gradientEndColor;
+  final double? height;
+  final double? width;
+  final Widget? vectorBottom;
+  final Widget? vectorTop;
+  final double? borderRadius;
+  final Widget? icon;
+  final Function? onTap;
+  const DiscoverLongCard(
+      {Key? key,
+      this.title,
+      this.subtitle,
+      this.gradientStartColor,
+      this.gradientEndColor,
+      this.height,
+      this.width,
+      this.vectorBottom,
+      this.vectorTop,
+      this.borderRadius,
+      this.icon,
+      this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(20),
+      onTap: () => onTap ?? () {},
+      child: Ink(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          gradient: LinearGradient(
+            colors: [
+              gradientStartColor ?? Color(0xff441DFC),
+              gradientEndColor ?? Color(0xff4E81EB),
+            ],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+        child: Stack(
+          children: [
+            Container(
+              height: 100.w,
+              width: 300.w,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: SizedBox(
+                height: 100.w,
+                width: 300.w,
+                child: Stack(
+                  children: [
+                    SizedBox(
+                      height: 100.w,
+                      width: 300.w,
+                      child: SvgAsset(assetName: AssetName.vectorSmallBottom),
+                    ),
+                    SizedBox(
+                      child: SvgAsset(
+                          height: 100.w,
+                          width: 300.w,
+                          fit: BoxFit.fitHeight,
+                          assetName: AssetName.vectorSmallTop),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 100.w,
+              width: 300.w,
+              child: Padding(
+                padding: EdgeInsets.only(
+                    left: 10.w, right: 0.w, top: 10.w, bottom: 10.w),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    SizedBox(
+                      width: 150.w,
+                      child: Text(
+                        title!,
+                        softWrap: true,
+                        style: TextStyle(
+                            fontSize: 20.w,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ),
+                    icon ??
+                        SvgAsset(
+                          assetName: AssetName.headphone,
+                          height: 24.w,
+                          width: 24.w,
+                        )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
